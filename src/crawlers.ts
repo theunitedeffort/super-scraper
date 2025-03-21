2bffd52969a63b279e527b31380bcea8825b77ab
trevorshannon <trevor.j.shannon@gmail.com>
2025-03-21 16:28:55 +0000

    Increase max retries to 3

--- src/crawlers.ts
+++ src/crawlers.ts
@@ -24,7 +24,7 @@ export const createAndStartCrawler = async (crawlerOptions: CrawlerOptions = DEF
     const crawler = new PlaywrightCrawler({
         keepAlive: true,
         proxyConfiguration: proxyConfig,
-        maxRequestRetries: 2,
+        maxRequestRetries: 3,
         requestQueue: queue,
         launchContext: {
             browserPerProxy: false,
